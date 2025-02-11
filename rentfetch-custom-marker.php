bf49513ccaf20e8eea230ca059e5a2fdfab65a4c
elodin <jon@elod.in>
2025-02-10 20:55:54 -0600

    Adding customization for where it goes

--- rentfetch-custom-marker.php
+++ rentfetch-custom-marker.php
@@ -2,26 +2,25 @@
 /*
 	Plugin Name: Rent Fetch Custom Marker
 	Plugin URI: https://elod.in
-    Description: Just another plugin
+	Description: Just another plugin
 	Version: 0.1
-    Author: Jon Schroeder
-    Author URI: https://elod.in
+	Author: Jon Schroeder
+	Author URI: https://elod.in
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 */
-
 
 /* Prevent direct access to the plugin */
 if ( !defined( 'ABSPATH' ) ) {
-    die( "Sorry, you are not allowed to access this page directly." );
+	die( "Sorry, you are not allowed to access this page directly." );
 }
 
 // Plugin directory
@@ -30,14 +29,26 @@ define( 'RFCM', dirname( __FILE__ ) );
 // Define the version of the plugin
 define ( 'RFCM_VERSION', '0.1' );
 
-
+/**
+ * Enqueue the main script.
+ *
+ * @return  [type]  [return description]
+ */
 function rfcustommarker_enqueue_script() {
-    wp_enqueue_script( 
-        'rfcustommarker', 
-        plugins_url( 'custom-marker.js', __FILE__ ), 
-        array( 'jquery', 'rentfetch-property-map' ), // Keep this dependency
-        RFCM_VERSION, 
-        true 
-    );
+	wp_register_script( 
+		'rfcustommarker', 
+		plugins_url( 'custom-marker.js', __FILE__ ), 
+		array( 'jquery', 'rentfetch-property-map' ), // Keep this dependency
+		RFCM_VERSION, 
+		true 
+	);
 }
 add_action( 'wp_enqueue_scripts', 'rfcustommarker_enqueue_script', 999 );
+
+/**
+ * Let's only enqueue the script in the right place on the site.
+ */
+function rfcustommarker_conditional_enqueue() {
+	wp_enqueue_script( 'rfcustommarker' );
+}
+add_action( 'init', 'rfcustommarker_conditional_enqueue' );
